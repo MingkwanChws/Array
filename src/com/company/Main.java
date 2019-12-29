071fbe621e9fa780b0b3c85885617a85b878e60c
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-29 14:11:05 +0700

    Add array and sum total

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,19 @@ public class Main {
         score5 = 30;
         int sum = score1 + score2 + score3 + score4 + score5;
         System.out.println(sum);
+
+        int[] scores = new int[5]; //0-4
+        scores[0] = 10;
+        scores[1] = 15;
+        scores[2] = 20;
+        scores[3] = 25;
+        scores[4] = 30;
+        int sum2 = 0;
+        for (int i = 0; i < 5; i++) {
+            sum2 = sum2 + scores[i];
+        }
+        System.out.println(sum2);
+
     }
 
     public static void main(String[] args) {
