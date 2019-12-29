4962b734aec8d8e2d67feae8c82c7efe0ee361bb
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-29 14:08:23 +0700

    Add sum total of score (not array)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,11 @@ public class Main {
         score3 = 20;
         score4 = 25;
         score5 = 30;
-
+        int sum = score1 + score2 + score3 + score4 + score5;
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
-        // write your code here
+        demo1();
     }
 }
