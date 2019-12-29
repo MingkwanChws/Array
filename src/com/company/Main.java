5c83883b57c9ed3ece155385307a4d555d9bf567
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-29 13:58:02 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public static void demo1() {
+        int score1, score2, score3, score4, score5;
+        score1 = 10;
+        score2 = 15;
+
+
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
