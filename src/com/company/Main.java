90f85885f72074ed2e4b044b85d535f67efb4c53
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-29 14:05:27 +0700

    Not array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,10 @@ public class Main {
         int score1, score2, score3, score4, score5;
         score1 = 10;
         score2 = 15;
+        score3 = 20;
+        score4 = 25;
+        score5 = 30;
 
-        int[] scores = new int[5]; //0-4
-        scores[0] = 10;
-        scores[1] = 15;
-        scores[2] = 20;
-        scores[3] = 25;
-        scores[4] = 30;
-
-
-
-        
     }
 
     public static void main(String[] args) {
